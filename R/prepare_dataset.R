86baf64ab8619fc4363308ccf32325a3e3615494
AlexWeinreb <alexis.weinreb@yale.edu>
2024-07-26 18:25:34 -0400

    Add event names, remove psi

--- R/prepare_dataset.R
+++ R/prepare_dataset.R
@@ -66,61 +66,21 @@
 # dpsi <- dpsi |>
 #   mutate(gene_name = i2s(gene_id, gids, warn_missing = TRUE))
 # 
+# 
+# 
+# 
+# set.seed(123)
+# table_event_names <- tibble(lsv_id = unique(dpsi$lsv_id),
+#                             event_name = babynames::babynames$name |> unique() |> sample(length(unique(dpsi$lsv_id))))
+# # write_csv(table_event_names, "data_use/240726_table_event_names.csv")
+# 
+# dpsi <- dpsi |>
+#   left_join(table_event_names,
+#             by = "lsv_id")
+# 
 # # save as DuckDB
 # con <- DBI::dbConnect(duckdb::duckdb(), "data_use/240726_dpsi.duckdb")
 # DBI::dbWriteTable(con, name = "dpsi",value = dpsi)
 # 
 # DBI::dbDisconnect(con)
-# 
-# 
-# 
-# #~ Load PSI ----
-# data_dir <- "data_raw/2024-03-04_outs/psi/"
-# 
-# files_psi <- list.files(data_dir,
-#                         pattern = "\\.tsv$",
-#                         full.names = FALSE)
-# 
-# 
-# psidta <- map_dfr(files_psi,
-#                   ~read_tsv(file.path(data_dir, .x),
-#                             col_names = c("gene_id", "lsv_id", "lsv_type",
-#                                           "mean_psi_per_lsv_junction", "stdev_psi_per_lsv_junction",
-#                                           "nb_sj", "nb_exons",
-#                                           "sj_coords", "ir_coords"),
-#                             col_types = cols(
-#                               gene_id = col_character(),
-#                               lsv_id = col_character(),
-#                               lsv_type = col_character(),
-#                               mean_psi_per_lsv_junction = col_character(),
-#                               stdev_psi_per_lsv_junction = col_character(),
-#                               nb_sj = col_double(),
-#                               nb_exons = col_double(),
-#                               sj_coords = col_character(),
-#                               ir_coords = col_character()
-#                             ),
-#                             skip = 1,
-#                             na = "na",
-#                             progress = FALSE) |>
-#                     add_column(neuron = str_split_1(.x, "\\.")[[1]]),
-#                   .progress = TRUE)
-# 
-# psi <- psidta |>
-#   separate_rows(mean_psi_per_lsv_junction, stdev_psi_per_lsv_junction,
-#                 sep = ";") |>
-#   group_by(neuron, lsv_id) |>
-#   mutate(junction_id = row_number()) |>
-#   ungroup()  |>
-#   mutate(across(c(mean_psi_per_lsv_junction,stdev_psi_per_lsv_junction), as.double),
-#          junction_id = factor(junction_id)) |>
-#   mutate(gene_name = i2s(gene_id, gids, warn_missing = TRUE))
-# 
-# 
-# # save as DuckDB
-# con <- DBI::dbConnect(duckdb::duckdb(), "data_use/240726_psi.duckdb")
-# DBI::dbWriteTable(con, name = "psi",value = psi)
-# 
-# DBI::dbDisconnect(con)
-# 
-# 
-# 
+
